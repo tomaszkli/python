38419887c5a8cd568dccc14a5dc40f98e64dc112
TomaszK <66512153+tomaszkli@users.noreply.github.com>
2020-06-28 08:12:25 +0200

    Update kwadrat.py

--- kwadrat.py
+++ kwadrat.py
@@ -1,43 +1,54 @@
 import turtle
-turtle.forward(300)
-turtle.left(90)
+import random
+a=["red", "green", "blue"]
+for i in range(8):
+ #losowanie jednego z trzech kolorów
+ fillcolor(a[0])
+ begin_fill()
 
-turtle.forward(300)
-turtle.left(90)
 
-turtle.forward(300)
-turtle.left(90)
 
-turtle.forward(300)
-turtle.left(90)
 
-turtle.forward(300/2)
-turtle.circle(300/2, steps=4)
-turtle.left(45)
-turtle.forward(106)
-turtle.left(45)
-turtle.forward(300/2)
-turtle.left(90)
-turtle.forward(300/2)
-turtle.left(90)
-turtle.forward(300/2)
-turtle.left(90)
-turtle.forward(150)
-turtle.left(90)
-turtle.forward(75)
-turtle.circle(75, steps=4)
-turtle.left(45)
-turtle.forward(54)
-turtle.left(45)
-turtle.forward(150/2)
-turtle.left(90)
-turtle.forward(150/2)
-turtle.left(90)
-turtle.forward(150/2)
-turtle.left(90)
-turtle.forward(150/2)
-turtle.left(90)
-turtle.forward(75/2)
-turtle.circle(38, steps=4)
 
-turtle.mainloop()
+def narysuj_wielobok(wielobok):
+    n = len(wielobok)
+    turtle.up()
+    turtle.goto(wielobok[0])
+    turtle.down()
+    for i in range(1, n + 1):
+        j = i % n
+        turtle.goto(wielobok[j])
+
+def oblicz_następny_wielobok(wielobok):
+    n = len(wielobok)
+    nowy_wielobok = []; 
+    for i in range(n):
+        j = (i + 1) % n
+        x1, y1 = wielobok[i]
+        x2, y2 = wielobok[j]
+        środek_odcinka = (x1 + x2) / 2,  (y1 + y2) / 2
+        nowy_wielobok.append(środek_odcinka)
+    return nowy_wielobok
+
+
+
+
+
+    
+
+def narysuj_wieloboki(wielobok, liczba_wieloboków_do_narysowania):
+    if liczba_wieloboków_do_narysowania > 0:
+        narysuj_wielobok(wielobok)    
+        nowy_wielobok = oblicz_następny_wielobok(wielobok)
+        narysuj_wieloboki(nowy_wielobok, liczba_wieloboków_do_narysowania - 1)
+        
+
+
+if __name__ == '__main__':
+   
+    liczba_kwadratów = 10
+    a = 300  # długość boku największego kwadratu
+    kwadrat = [ (0, 0), (a, 0), (a, a), (0, a) ]
+    narysuj_wieloboki(kwadrat, liczba_kwadratów)
+    
+    turtle.done()
